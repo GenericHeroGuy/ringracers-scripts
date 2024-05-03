6d7fbeefe9c30c7d3caaccf0aaa1fccaa7e66668
Indev <bdmitry450@gmail.com>
2024-05-03 23:47:58 +0300

    Itemboxes: allow tweaking amount of time needed for ringbox to become itembox (itembox_ringboxtime cvar) Removed disable_ringboxes cvar, to disable ringboxes set itembox_ringboxtime to 0

--- src/sglua/Lua/itemboxes.lua
+++ src/sglua/Lua/itemboxes.lua
@@ -1,11 +1,3 @@
-local cv_disable_ringboxes = CV_RegisterVar({
-    name = "disable_ringboxes",
-    defaultvalue = "Off",
-    flags = CV_NETVAR,
-    possiblevalue = CV_OnOff,
-    description = "Disables ringboxes",
-})
-
 local cv_itembox_fuse = CV_RegisterVar({
     name = "itembox_fuse",
     defaultvalue = TICRATE/4,
@@ -14,14 +6,28 @@ local cv_itembox_fuse = CV_RegisterVar({
     description = "Time before ring/item box will respawn after being picked up",
 })
 
+local cv_itembox_ringboxtime = CV_RegisterVar({
+    name = "itembox_ringboxtime",
+    defaultvalue = "1.5",
+    flags = CV_NETVAR|CV_FLOAT,
+    possiblevalue = { MIN = 0, MAX = 3*FRACUNIT },
+    description = "Time before ring box becomes item box again"
+})
+
 local S_RINGBOX1 = S_RINGBOX1
 local S_RINGBOX12 = S_RINGBOX12
 
+local function FixedTics(fixed)
+    return fixed*TICRATE/FRACUNIT
+end
+
 -- No ringboxes
 addHook("MobjThinker", function(mo)
-    if cv_disable_ringboxes.value then
-        if mo.state >= S_RINGBOX1 and mo.state <= S_RINGBOX12 then
+    if mo.state >= S_RINGBOX1 and mo.state <= S_RINGBOX12 then
+        if cv_itembox_ringboxtime.value == 0 then
             mo.state = S_RANDOMITEM1
+        else
+            mo.extravalue1 = max($, 3*TICRATE-FixedTics(cv_itembox_ringboxtime.value))
         end
     end
 
