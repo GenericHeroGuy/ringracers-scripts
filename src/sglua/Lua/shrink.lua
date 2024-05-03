57b758d542c4c00dc4c0dd8175fbefbd5fdef9b4
Indev <bdmitry450@gmail.com>
2024-05-03 23:02:57 +0300

    Add shrink.lua

---
+++ src/sglua/Lua/shrink.lua
@@ -0,0 +1,11 @@
+local cv_shrinktime = CV_RegisterVar({
+    name = "shrinktime",
+    defaultvalue = "10",
+    possiblevalue = { MIN = 1, MAX = 30 },
+    flags = CV_NETVAR,
+    description = "Shrink item duration"
+})
+
+addHook("MobjThinker", function(mo)
+    mo.reactiontime = min($, cv_shrinktime.value*TICRATE)
+end, MT_SHRINK_POHBEE)
